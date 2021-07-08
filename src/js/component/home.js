ff79a547bc2c921c8e24b1144b11d497fd9941c8
Mappel21 <Mappel7676@gmail.com>
2021-07-08 00:42:42 +0000

    set todo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,20 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+
+	const [list, setList] = useState([]);
+	const [todo, setTodo] = useState("");
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>To-Do List</h1>
+			<input
+				value={todo}
+				className="input"
+				placeholder="What needs to be done?"
+				onChange={e => setTodo(e.todo.value)}
+				/>
 		</div>
 	);
 }
